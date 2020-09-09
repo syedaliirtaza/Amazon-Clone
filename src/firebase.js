7e25692565d61b2904166a04fcbfcff21bbdae2e
syedaliirtaza <aliirtaza401@gmail.com>
2020-09-09 05:08:58 -0700

    added the code for the nav bar of our amazon clone

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBLtms_goCYBvrvdyy1BLyOeOWXqbCH97k",
+  authDomain: "clone-38ccf.firebaseapp.com",
+  databaseURL: "https://clone-38ccf.firebaseio.com",
+  projectId: "clone-38ccf",
+  storageBucket: "clone-38ccf.appspot.com",
+  messagingSenderId: "822353029281",
+  appId: "1:822353029281:web:829ddd0feba7a0e16068e0"
+};
